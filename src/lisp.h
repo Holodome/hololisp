e85e4b9961f312b70c643f12869392d6b78c7379
Holodome <ilal.vinogradov@gmail.com>
2022-05-31 19:20:28 +0300

    create lisp type definitions and basic api

---
+++ src/lisp.h
@@ -0,0 +1,73 @@
+#ifndef __HLL_LISP_H__
+#define __HLL_LISP_H__
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct hll_lexer;
+
+typedef enum {
+    HLL_LOBJ_NONE = 0x0,
+    HLL_LOBJ_CONS = 0x1,
+    HLL_LOBJ_SYMB = 0x2,
+    /* HLL_LOBJ_FUNC  = 0x3, */
+    /* HLL_LOBJ_MACRO = 0x4, */
+    /* HLL_LOBJ_TRUE  = 0x5, */
+    /* HLL_LOBJ_NIL   = 0x6, */
+    HLL_LOBJ_INT = 0x7
+} hll_lisp_obj_kind;
+
+typedef struct hll_lisp_obj_head {
+    hll_lisp_obj_kind kind;
+
+    size_t size;
+} hll_lisp_obj_head;
+
+typedef struct {
+    hll_lisp_obj_head  head;
+    hll_lisp_obj_head *car;
+    hll_lisp_obj_head *cdr;
+} hll_lisp_obj_cons;
+
+typedef struct {
+    hll_lisp_obj_head head;
+    char const       *symb;
+} hll_lisp_obj_symb;
+
+typedef struct {
+    hll_lisp_obj_head  head;
+    hll_lisp_obj_head *params;
+    hll_lisp_obj_head *body;
+    hll_lisp_obj_head *env;
+} hll_lisp_obj_func;
+
+typedef struct {
+    hll_lisp_obj_head head;
+    int64_t           value;
+} hll_lisp_obj_int;
+
+/* extern hll_lisp_obj_head *hll_true; */
+extern hll_lisp_obj_head *hll_nil;
+
+#define HLL_LOBJ_ALLOC(_name) \
+    hll_lisp_obj_head *_name(size_t size, hll_lisp_obj_kind kind)
+typedef HLL_LOBJ_ALLOC(hll_lisp_obj_alloc);
+
+#define HLL_LOBJ_FREE(_name) void *_name(hll_lisp_obj_head *head)
+typedef HLL_LOBJ_FREE(hll_lisp_obj_free);
+
+typedef struct {
+    hll_lisp_obj_alloc *alloc;
+    hll_lisp_obj_free  *free;
+    void               *state;
+} hll_lisp_runtime;
+
+hll_lisp_obj_head *hll_make_cons(hll_lisp_runtime *ctx, hll_lisp_obj_head *car,
+                                 hll_lisp_obj_head *cdr);
+
+hll_lisp_obj_head *hll_make_symb(hll_lisp_runtime *ctx, char const *symb,
+                                 size_t length);
+
+hll_lisp_obj_head *hll_make_int(hll_lisp_runtime *ctx, int64_t value);
+
+#endif
