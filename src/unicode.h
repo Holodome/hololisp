e19e51376d8ad2f5e1b152a74aafdb4537b30c88
Holodome <ilal.vinogradov@gmail.com>
2022-05-31 16:03:06 +0300

    update unicode files

--- src/unicode.h
+++ src/unicode.h
@@ -3,25 +3,17 @@
 
 #include <stdint.h>
 
-/* 
- * Decodes from utf8 unicode codepoint
- * Retuns pointer to byte past decoded codepoint.
- * NOTE: Source buffer must always have 4 bytes aviable for reading.
- * This means that 3 additional zero bytes must be added to the end
- */
-void const *utf8_decode_fast(void const *src, uint32_t *codepoint_p);
+/// \brief Decodes unicode codepoint from utf8-encoded data stream.
+/// \param src Data to decode from
+/// \param utf32 Pointer where decoded codepoint should be written.
+/// \returns Data pointer past read codepoint. If data point is not correct
+/// utf8-encoded data pointer is not changed.
+void const *utf8_decode(void const *src, uint32_t *utf32);
 
-/*
- * Decodes unciode codepoint from utf8.
- * Returns pointer past read utf8 sequnce and writes codepoint to codepoint_p.
- * If given source is not correct utf8 sequnce, codepoint 0xFFFFFFFF is written
- */
-void const *utf8_decode(void const *src, uint32_t *codepoint_p);
-
-/*
- * Encodes unciode codepoint as utf8.
- * Writes encoded bytes to dst, returns pointer past last written byte
- */
+/// \brief Encodes unicode codepoint to utf8.
+/// \param dst Utf8-encoded destination buffer. Must contain at least 4 bytes.
+/// \param codepoint utf32 codepoint to encode to utf8.
+/// \returns Pointer one byte past encoded data.
 void *utf8_encode(void *dst, uint32_t codepoint);
 
 #endif
