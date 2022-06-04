302068501d5b4e77babcb542dc3afbe7a457de3a
Holodome <ilal.vinogradov@gmail.com>
2022-06-04 22:48:27 +0300

    src/lisp.h: Add bindings

--- src/lisp.h
+++ src/lisp.h
@@ -5,6 +5,7 @@
 #include <stdint.h>
 
 struct hll_lexer;
+struct hll_lisp_ctx;
 
 #define HLL_BUILTIN_QUOTE_SYMB_NAME "quote"
 
@@ -12,11 +13,9 @@ typedef enum {
     HLL_LOBJ_NONE = 0x0,
     HLL_LOBJ_CONS = 0x1,
     HLL_LOBJ_SYMB = 0x2,
-    /* HLL_LOBJ_FUNC  = 0x3, */
-    /* HLL_LOBJ_MACRO = 0x4, */
-    /* HLL_LOBJ_TRUE  = 0x5, */
-    HLL_LOBJ_NIL = 0x6,
-    HLL_LOBJ_INT = 0x7
+    HLL_LOBJ_NIL = 0x3,
+    HLL_LOBJ_INT = 0x4,
+    HLL_LOBJ_BIND = 0x5
 } hll_lisp_obj_kind;
 
 typedef struct hll_lisp_obj_head {
@@ -44,6 +43,14 @@ typedef struct {
     int64_t value;
 } hll_lisp_int;
 
+#define HLL_LISP_BIND(_name) \
+    hll_lisp_obj_head *_name(struct hll_lisp_ctx *ctx, hll_lisp_obj_head *args)
+typedef HLL_LISP_BIND(hll_lisp_bind_func);
+
+typedef struct {
+    hll_lisp_bind_func *binding;
+} hll_lisp_bind;
+
 #define HLL_LOBJ_ALLOC(_name) \
     hll_lisp_obj_head *_name(size_t body_size, hll_lisp_obj_kind kind)
 typedef HLL_LOBJ_ALLOC(hll_lisp_obj_alloc);
@@ -69,7 +76,11 @@ hll_lisp_symb *hll_unwrap_symb(hll_lisp_obj_head *obj);
 
 hll_lisp_int *hll_unwrap_int(hll_lisp_obj_head *obj);
 
+hll_lisp_bind *hll_unwrap_bind(hll_lisp_obj_head *obj);
+
 hll_lisp_ctx hll_default_ctx(void);
+
+void hll_add_builtins(hll_lisp_ctx *ctx);
 
 hll_lisp_obj_head *hll_make_cons(hll_lisp_ctx *ctx, hll_lisp_obj_head *car,
                                  hll_lisp_obj_head *cdr);
@@ -81,6 +92,8 @@ hll_lisp_obj_head *hll_make_symb(hll_lisp_ctx *ctx, char const *symb,
                                  size_t length);
 
 hll_lisp_obj_head *hll_make_int(hll_lisp_ctx *ctx, int64_t value);
+
+hll_lisp_obj_head *hll_make_binding(hll_lisp_ctx *ctx, hll_lisp_bind_func *bind);
 
 hll_lisp_obj_head *hll_reverse_list(hll_lisp_obj_head *obj);
 
