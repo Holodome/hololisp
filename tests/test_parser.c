81e7647d30899788f532cca402b43fdca27fac39
Holodome <ilal.vinogradov@gmail.com>
2022-06-04 22:06:15 +0300

    tests/test_parser.c: Add test for parsing nested lists

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -68,18 +68,6 @@ test_parser_parses_symbol(void) {
     TEST_ASSERT(result == HLL_PARSE_EOF);
 }
 
-#if 0
-static void
-test_parser_parses_quoted_symbol(void) {
-    TEST_ASSERT(0);
-}
-
-static void
-test_parser_parses_quoted_int(void) {
-    TEST_ASSERT(0);
-}
-#endif 
-
 static void
 test_parser_parses_one_element_list(void) {
     char const *source = "(100)";
@@ -99,7 +87,7 @@ test_parser_parses_one_element_list(void) {
     TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
     hll_lisp_cons *cons = hll_unwrap_cons(obj);
     TEST_ASSERT(cons->cdr == hll_nil);
-    
+
     hll_lisp_obj_head *car = cons->car;
     TEST_ASSERT(car->kind == HLL_LOBJ_INT);
     TEST_ASSERT(hll_unwrap_int(car)->value == 100);
@@ -126,7 +114,7 @@ test_parser_parses_list(void) {
     TEST_ASSERT(obj != NULL);
     TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
     hll_lisp_cons *cons = hll_unwrap_cons(obj);
-    
+
     hll_lisp_obj_head *car = cons->car;
     TEST_ASSERT(car->kind == HLL_LOBJ_INT);
     TEST_ASSERT(hll_unwrap_int(car)->value == -100);
@@ -155,11 +143,97 @@ test_parser_parses_list(void) {
     TEST_ASSERT(result == HLL_PARSE_EOF);
 }
 
+static void
+test_parser_parses_nested_lists(void) {
+    char const *source = "(+ (* 3 2) hello)";
+    char buffer[4096];
+
+    hll_lisp_ctx ctx = hll_default_ctx();
+    hll_init_libc_no_gc(&ctx);
+
+    hll_lexer lexer = hll_lexer_create(source, buffer, sizeof(buffer));
+    hll_parser parser = hll_parser_create(&lexer, &ctx);
+
+    hll_lisp_obj_head *obj = NULL;
+
+    hll_parse_result result = hll_parse(&parser, &obj);
+    TEST_ASSERT(result == HLL_PARSE_OK);
+    TEST_ASSERT(obj != NULL);
+    {
+        TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
+        hll_lisp_cons *cons = hll_unwrap_cons(obj);
+        hll_lisp_obj_head *car = cons->car;
+        TEST_ASSERT(car->kind == HLL_LOBJ_SYMB);
+        TEST_ASSERT(strcmp(hll_unwrap_symb(car)->symb, "+") == 0);
+
+        obj = cons->cdr;
+        TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
+        cons = hll_unwrap_cons(obj);
+        car = cons->car;
+
+        hll_lisp_cons *old_cons = cons;
+
+        {
+            TEST_ASSERT(car->kind == HLL_LOBJ_CONS);
+            cons = hll_unwrap_cons(car);
+
+            car = cons->car;
+            TEST_ASSERT(car->kind == HLL_LOBJ_SYMB);
+            TEST_ASSERT(strcmp(hll_unwrap_symb(car)->symb, "*") == 0);
+
+            TEST_ASSERT(cons->cdr != hll_nil);
+            obj = cons->cdr;
+            TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
+            cons = hll_unwrap_cons(obj);
+
+            car = cons->car;
+            TEST_ASSERT(car->kind == HLL_LOBJ_INT);
+            TEST_ASSERT(hll_unwrap_int(car)->value == 3);
+
+            TEST_ASSERT(cons->cdr != hll_nil);
+            obj = cons->cdr;
+            TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
+            cons = hll_unwrap_cons(obj);
+
+            car = cons->car;
+            TEST_ASSERT(car->kind == HLL_LOBJ_INT);
+            TEST_ASSERT(hll_unwrap_int(car)->value == 2);
+
+            TEST_ASSERT(cons->cdr == hll_nil);
+        }
+
+        cons = old_cons;
+        TEST_ASSERT(cons->cdr != hll_nil);
+        obj = cons->cdr;
+        TEST_ASSERT(obj->kind == HLL_LOBJ_CONS);
+        cons = hll_unwrap_cons(obj);
+
+        car = cons->car;
+        TEST_ASSERT(car->kind == HLL_LOBJ_SYMB);
+        TEST_ASSERT(strcmp(hll_unwrap_symb(car)->symb, "hello") == 0);
+
+        TEST_ASSERT(cons->cdr == hll_nil);
+    }
+
+    result = hll_parse(&parser, &obj);
+    TEST_ASSERT(result == HLL_PARSE_EOF);
+}
+
 #if 0
 static void
 test_parser_parses_quoted_list(void) {
 }
-#endif 
+
+static void
+test_parser_parses_quoted_symbol(void) {
+    TEST_ASSERT(0);
+}
+
+static void
+test_parser_parses_quoted_int(void) {
+    TEST_ASSERT(0);
+}
+#endif
 
 #define TCASE(_name) \
     { #_name, _name }
@@ -169,8 +243,9 @@ TEST_LIST = { TCASE(test_parser_reports_eof),
               TCASE(test_parser_parses_symbol),
               /* TCASE(test_parser_parses_quoted_symbol), */
               /* TCASE(test_parser_parses_quoted_int), */
+              /* TCASE(test_parser_parses_quoted_list), */
               TCASE(test_parser_parses_one_element_list),
               TCASE(test_parser_parses_list),
-              /* TCASE(test_parser_parses_quoted_list), */
+              TCASE(test_parser_parses_nested_lists),
               { NULL, NULL } };
 
