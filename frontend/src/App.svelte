32caebbd00cd0b3593d93febdb67be92b4e514bb
Holodome <ilal.vinogradov@gmail.com>
2022-06-29 15:21:46 +0300

    Create footer with github link

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,11 @@
 <script>
   import Header from "./Header.svelte";
+  import Editor from "./Editor/Editor.svelte";
+  import Footer from "./Footer.svelte";
 </script>
 
 <main>
   <Header />
+  <Editor />
+  <Footer />
 </main>
