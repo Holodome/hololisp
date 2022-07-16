b7512d5cfb4249e6ec03c2a7b4ebb369977e38ef
Holodome <ilal.vinogradov@gmail.com>
2022-07-16 11:24:44 +0300

    Comment bytecode in tests

--- tests/test_compiler.c
+++ tests/test_compiler.c
@@ -11,30 +11,24 @@ test_compiler_compiles_integer(void) {
 
     hll_bytecode *result = hll_compile(vm, source);
     TEST_ASSERT(result != NULL);
-
-    TEST_ASSERT(memcmp(result->ops, bytecode, sizeof(bytecode)) == 0);
+    2§ TEST_ASSERT(memcmp(result->ops, bytecode, sizeof(bytecode)) == 0);
 }
 
 static void
 test_compiler_compiles_addition(void) {
     char const *source = "(+ 1 2)";
-    uint8_t bytecode[] = { HLL_BYTECODE_SYMB,
-                           0x00,
-                           0x00,
-                           HLL_BYTECODE_FIND,
-                           HLL_BYTECODE_NIL,
-                           HLL_BYTECODE_NIL,
-                           HLL_BYTECODE_CONST,
-                           0x00,
-                           0x00,
-                           HLL_BYTECODE_APPEND,
-                           HLL_BYTECODE_CONST,
-                           0x00,
-                           0x01,
-                           HLL_BYTECODE_APPEND,
+    uint8_t bytecode[] = { // +
+                           HLL_BYTECODE_SYMB, 0x00, 0x00, HLL_BYTECODE_FIND,
+                           // (1 2)
+                           HLL_BYTECODE_NIL, HLL_BYTECODE_NIL,
+                           // 1
+                           HLL_BYTECODE_CONST, 0x00, 0x00, HLL_BYTECODE_APPEND,
+                           // 2
+                           HLL_BYTECODE_CONST, 0x00, 0x01, HLL_BYTECODE_APPEND,
                            HLL_BYTECODE_POP,
-                           HLL_BYTECODE_CALL,
-                           HLL_BYTECODE_END };
+                           // (+ 1 2)
+                           HLL_BYTECODE_CALL, HLL_BYTECODE_END
+    };
     hll_vm *vm = hll_make_vm(NULL);
 
     hll_bytecode *result = hll_compile(vm, source);
@@ -47,51 +41,65 @@ static void
 test_compiler_compiles_complex_arithmetic_operation(void) {
     char const *source = "(+ (* 3 5) 2 (/ 2 1))";
     uint8_t bytecode[] = {
+        // +
         HLL_BYTECODE_SYMB,
         0x00,
         0x00,
         HLL_BYTECODE_FIND,
+        // ((* 3 5) 2 (/ 2 1))
         HLL_BYTECODE_NIL,
         HLL_BYTECODE_NIL,
+        // *
         HLL_BYTECODE_SYMB,
         0x00,
         0x01,
         HLL_BYTECODE_FIND,
+        // (3 5)
         HLL_BYTECODE_NIL,
         HLL_BYTECODE_NIL,
+        // 3
         HLL_BYTECODE_CONST,
         0x00,
         0x00,
         HLL_BYTECODE_APPEND,
+        // 5
         HLL_BYTECODE_CONST,
         0x00,
         0x01,
         HLL_BYTECODE_APPEND,
         HLL_BYTECODE_POP,
+        // (* 3 5)
         HLL_BYTECODE_CALL,
         HLL_BYTECODE_APPEND,
+        // 2
         HLL_BYTECODE_CONST,
         0x00,
         0x02,
         HLL_BYTECODE_APPEND,
+        // /
         HLL_BYTECODE_SYMB,
         0x00,
         0x02,
         HLL_BYTECODE_FIND,
+        // (2 1)
         HLL_BYTECODE_NIL,
         HLL_BYTECODE_NIL,
+        // 2
         HLL_BYTECODE_CONST,
         0x00,
         0x02,
         HLL_BYTECODE_APPEND,
+        // 1
         HLL_BYTECODE_CONST,
         0x00,
         0x03,
         HLL_BYTECODE_APPEND,
         HLL_BYTECODE_POP,
+        // (/ 2 1)
         HLL_BYTECODE_CALL,
         HLL_BYTECODE_APPEND,
         HLL_BYTECODE_POP,
+        // (+ (* 3 5) 2 (/ 2 1))
         HLL_BYTECODE_CALL,
         HLL_BYTECODE_END,
 
@@ -116,21 +124,14 @@ test_compiler_compiles_if(void) {
 6:#E    END
      */
     char const *source = "(if t 1 0)";
-    uint8_t bytecode[] = { HLL_BYTECODE_TRUE,
-                           HLL_BYTECODE_JN,
-                           0x00,
-                           0x09,
-                           HLL_BYTECODE_CONST,
-                           0x00,
-                           0x00,
-                           HLL_BYTECODE_TRUE,
-                           HLL_BYTECODE_JT,
-                           0x00,
-                           0x05,
-                           HLL_BYTECODE_CONST,
-                           0x00,
-                           0x01,
-                           HLL_BYTECODE_END };
+    uint8_t bytecode[] = { // t
+                           HLL_BYTECODE_TRUE, HLL_BYTECODE_JN, 0x00, 0x09,
+                           // 1
+                           HLL_BYTECODE_CONST, 0x00, 0x00, HLL_BYTECODE_TRUE,
+                           HLL_BYTECODE_JT, 0x00, 0x05,
+                           // 0
+                           HLL_BYTECODE_CONST, 0x00, 0x01, HLL_BYTECODE_END
+    };
     hll_vm *vm = hll_make_vm(NULL);
 
     hll_bytecode *result = hll_compile(vm, source);
@@ -142,16 +143,11 @@ test_compiler_compiles_if(void) {
 static void
 test_compiler_compiles_quote(void) {
     char const *source = "'(1 2)";
-    uint8_t bytecode[] = { HLL_BYTECODE_NIL,
-                           HLL_BYTECODE_NIL,
-                           HLL_BYTECODE_CONST,
-                           0x00,
-                           0x00,
-                           HLL_BYTECODE_APPEND,
-                           HLL_BYTECODE_CONST,
-                           0x00,
-                           0x01,
-                           HLL_BYTECODE_APPEND,
+    uint8_t bytecode[] = { HLL_BYTECODE_NIL, HLL_BYTECODE_NIL,
+                           // 1
+                           HLL_BYTECODE_CONST, 0x00, 0x00, HLL_BYTECODE_APPEND,
+                           // 2
+                           HLL_BYTECODE_CONST, 0x00, 0x01, HLL_BYTECODE_APPEND,
                            HLL_BYTECODE_POP };
     hll_vm *vm = hll_make_vm(NULL);
 
@@ -188,8 +184,10 @@ test_lambda_application_working(void) {
     TEST_ASSERT(memcmp(result->ops, bytecode, sizeof(bytecode)) == 0);
 }
 
-#define TCASE(_name) \
-    { #_name, _name }
+#define TCASE(_name)  \
+    {                 \
+#_name, _name \
+    }
 
 TEST_LIST = { TCASE(test_compiler_compiles_integer),
               TCASE(test_compiler_compiles_addition),
